3f51fd4b311583fee47e206d71b350bef0d184c5
katanaevav <katanaev@gmail.com>
2020-06-23 23:06:59 +0400

    Управление плеером вынесено в родительские компоненты

--- src/components/artist-question-screen/artist-question-screen.jsx
+++ src/components/artist-question-screen/artist-question-screen.jsx
@@ -1,46 +1,60 @@
-import React from "react";
+import React, {PureComponent} from "react";
 import PropTypes from "prop-types";
 import {GameType} from "../../const.js";
 import AudioPlayer from "../audio-player/audio-player.jsx";
 
-const ArtistQuestionScreen = (props) => {
-  const {onAnswer, question} = props;
-  const {
-    answers,
-    song,
-  } = question;
 
-  return (
-    <section className="game__screen">
-      <h2 className="game__title">Кто исполняет эту песню?</h2>
-      <div className="game__track">
-        <div className="track">
-          <AudioPlayer
-            isPlaying={true}
-            src={song.src}
-          />
-        </div>
-      </div>
+class ArtistQuestionScreen extends PureComponent {
+  constructor(props) {
+    super(props);
 
-      <form className="game__artist">
-        {answers.map((answer, i) => (
-          <div key={answer.artist} className="artist">
-            <input className="artist__input visually-hidden" type="radio" name="answer" value={`answer-${i}`} id={`answer-${i}`}
-              onChange={(evt) => {
-                evt.preventDefault();
-                onAnswer(question, answer);
-              }}
+    this.state = {
+      isPlaying: true,
+    };
+  }
+
+  render() {
+    const {isPlaying} = this.state;
+    const {onAnswer, question} = this.props;
+    const {
+      answers,
+      song,
+    } = question;
+
+    return (
+      <section className="game__screen">
+        <h2 className="game__title">Кто исполняет эту песню?</h2>
+        <div className="game__track">
+          <div className="track">
+            <AudioPlayer
+              isPlaying={isPlaying}
+              src={song.src}
+              onPlayButtonClick={() => this.setState({isPlaying: !isPlaying})}
             />
-            <label className="artist__name" htmlFor={`answer-${i}`}>
-              <img className="artist__picture" src={answer.picture} alt={answer.artist} />
-              {answer.artist}
-            </label>
           </div>
-        ))}
-      </form>
-    </section>
-  );
-};
+        </div>
+
+        <form className="game__artist">
+          {answers.map((answer, i) => (
+            <div key={answer.artist} className="artist">
+              <input className="artist__input visually-hidden" type="radio" name="answer" value={`answer-${i}`} id={`answer-${i}`}
+                onChange={(evt) => {
+                  evt.preventDefault();
+                  onAnswer(question, answer);
+                }}
+              />
+              <label className="artist__name" htmlFor={`answer-${i}`}>
+                <img className="artist__picture" src={answer.picture} alt={answer.artist} />
+                {answer.artist}
+              </label>
+            </div>
+          ))}
+        </form>
+      </section>
+    );
+  }
+}
+
 
 ArtistQuestionScreen.propTypes = {
   onAnswer: PropTypes.func.isRequired,
@@ -56,5 +70,6 @@ ArtistQuestionScreen.propTypes = {
     type: PropTypes.oneOf([GameType.ARTIST, GameType.GENRE]).isRequired,
   }).isRequired,
 };
+
 
 export default ArtistQuestionScreen;
--- src/components/audio-player/audio-player.jsx
+++ src/components/audio-player/audio-player.jsx
@@ -51,6 +51,7 @@ export default class AudioPlayer extends PureComponent {
 
   render() {
     const {isLoading, isPlaying} = this.state;
+    const {onPlayButtonClick} = this.props;
 
     return (
       <Fragment>
@@ -58,7 +59,10 @@ export default class AudioPlayer extends PureComponent {
           className={`track__button track__button--${isPlaying ? `pause` : `play`}`}
           type="button"
           disabled={isLoading}
-          onClick={() => this.setState({isPlaying: !this.state.isPlaying})}
+          onClick={() => {
+            this.setState({isPlaying: !this.state.isPlaying});
+            onPlayButtonClick();
+          }}
         />
         <div className="track__status">
           <audio
@@ -82,5 +86,6 @@ export default class AudioPlayer extends PureComponent {
 
 AudioPlayer.propTypes = {
   isPlaying: PropTypes.bool.isRequired,
+  onPlayButtonClick: PropTypes.func.isRequired,
   src: PropTypes.string.isRequired,
 };
