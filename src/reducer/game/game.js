fd06e969e1dd741cbf3defb7f52f67db7be901c5
katanaevav <katanaev@gmail.com>
2020-07-25 14:25:28 +0400

    Обновлен тест

--- src/reducer/game/game.js
+++ src/reducer/game/game.js
@@ -32,6 +32,7 @@ const ActionCreator = {
 
   incrementMistake: (question, userAnswer) => {
     let answerIsCorrect = false;
+
     switch (question.type) {
       case GameType.ARTIST:
         answerIsCorrect = isArtistAnswerCorrect(question, userAnswer);
@@ -40,6 +41,7 @@ const ActionCreator = {
         answerIsCorrect = isGenreAnswerCorrect(question, userAnswer);
         break;
     }
+
     return {
       type: ActionType.INCREMENT_MISTAKES,
       payload: answerIsCorrect ? 0 : 1,
@@ -67,20 +69,25 @@ const reducer = (state = initialState, action) => {
       return extend(state, {
         step: state.step + action.payload,
       });
+
     case ActionType.INCREMENT_MISTAKES:
       return extend(state, {
         mistakes: state.mistakes + action.payload,
       });
+
     case ActionType.RESET:
       return extend(initialState, {
         step: 0,
       });
+
     case ActionType.GO_TO_WELCOME:
       return extend(initialState, {
         step: -1,
       });
   }
+
   return state;
 };
+
 
 export {reducer, ActionType, ActionCreator};
